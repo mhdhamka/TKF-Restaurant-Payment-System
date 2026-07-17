4b9b53f46f69a2c3685067d289d771704693071a
Mohd Hamka <122353752+mhdhamka@users.noreply.github.com>
2026-07-17 17:32:10 +0800

    Refactor admin panel functions in admin.c

--- src/admin.c
+++ src/admin.c
@@ -1,6 +1,5 @@
 #include "tkf.h"
 
-
 void adminPanel(Menu menu[], int *menuCount)
 {
     int choice;
@@ -26,47 +25,29 @@ void adminPanel(Menu menu[], int *menuCount)
 
         clearInputBuffer();
 
-
-
         switch(choice)
         {
             case 1:
-
                 addMenuItem(menu, menuCount);
-
                 break;
-
 
             case 2:
-
                 updateMenuPrice(menu,*menuCount);
-
                 break;
-
 
             case 3:
-
                 deleteMenuItem(menu, menuCount);
-
                 break;
-
 
             case 4:
-
                 salesReport(menu,*menuCount);
-
                 break;
-
 
             case 5:
-
                 printf("Returning to main menu...\n");
-
                 break;
 
-
             default:
-
                 printf("Invalid option.\n");
         }
 
@@ -75,12 +56,8 @@ void adminPanel(Menu menu[], int *menuCount)
 
 }
 
-
-
-
 void addMenuItem(Menu menu[], int *menuCount)
 {
-
     if(*menuCount >= MAX_MENU)
     {
         printf("Menu is full.\n");
@@ -89,75 +66,44 @@ void addMenuItem(Menu menu[], int *menuCount)
 
 
     Menu newItem;
-
-
     newItem.id = *menuCount + 1;
 
-
     printf("\nEnter food name: ");
-
+    
     fgets(newItem.name,sizeof(newItem.name),stdin);
-
     newItem.name[strcspn(newItem.name,"\n")] = '\0';
-
-
 
     printf("Enter category: ");
 
     fgets(newItem.category,sizeof(newItem.category),stdin);
-
     newItem.category[strcspn(newItem.category,"\n")] = '\0';
-
-
 
     printf("Enter regular price: ");
 
     scanf("%f",&newItem.regularPrice);
 
-
-
     printf("Enter large price: ");
 
     scanf("%f",&newItem.largePrice);
 
-
     clearInputBuffer();
 
-
-
     newItem.quantitySold = 0;
-
-
-
     menu[*menuCount] = newItem;
-
-
     (*menuCount)++;
-
-
 
     printf("\nMenu item added successfully.\n");
 
 }
 
-
-
-
-
 void updateMenuPrice(Menu menu[], int menuCount)
 {
-
     int id;
-
 
     displayMenu(menu,menuCount);
 
-
     printf("\nEnter menu ID to update: ");
-
     scanf("%d",&id);
-
-
 
     if(id < 1 || id > menuCount)
     {
@@ -165,55 +111,35 @@ void updateMenuPrice(Menu menu[], int menuCount)
         return;
     }
 
-
-
     printf("New regular price: ");
 
     scanf("%f",&menu[id-1].regularPrice);
-
-
 
     printf("New large price: ");
 
     scanf("%f",&menu[id-1].largePrice);
 
-
-
     clearInputBuffer();
-
-
 
     printf("Price updated successfully.\n");
 
 }
 
-
-
-
-
 void deleteMenuItem(Menu menu[], int *menuCount)
 {
-
     int id;
 
-
     displayMenu(menu,*menuCount);
-
-
 
     printf("\nEnter menu ID to delete: ");
 
     scanf("%d",&id);
-
-
 
     if(id < 1 || id > *menuCount)
     {
         printf("Invalid menu ID.\n");
         return;
     }
-
-
 
     for(int i=id-1;i<(*menuCount)-1;i++)
     {
@@ -222,19 +148,11 @@ void deleteMenuItem(Menu menu[], int *menuCount)
         menu[i].id=i+1;
     }
 
-
-
     (*menuCount)--;
-
-
 
     printf("Menu item deleted successfully.\n");
 
 }
-
-
-
-
 
 void salesReport(Menu menu[], int menuCount)
 {
@@ -250,10 +168,7 @@ void salesReport(Menu menu[], int menuCount)
            "Food",
            "Sold");
 
-
     printf("------------------------------------\n");
-
-
 
     for(int i=0;i<menuCount;i++)
     {
